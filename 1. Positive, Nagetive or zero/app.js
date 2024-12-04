26a59c14b0db92fd4ea1ac09a58b7e97d1e109dd
dalveersingh <dalveerchu1@gmail.com>
2024-12-04 07:40:38 +0530

    Positive nagetive or zerot

---
+++ 1. Positive, Nagetive or zero/app.js
@@ -0,0 +1,14 @@
+// program that checks if the number is positive, negative or zero
+let number = prompt("Enter number: ");
+
+if (number > 0) {
+    document.write("positive");
+}
+
+else if (number == 0) {
+  document.write("zero");
+}
+else {
+  document.write("negative");
+}
+
