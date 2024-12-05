4982c2c9e1628da1d830a6aa570d93e9202dc83a
dalveersingh <dalveerchu1@gmail.com>
2024-12-06 04:00:40 +0530

    hoisting program

---
+++ 12.function two value sum/app.js
@@ -0,0 +1,11 @@
+function sum(a, b) {
+  const sum = a + b;
+  console.log("sum id numbers", sum);
+}
+sum(1, 2);
+sum(122, 223);
+sum(-1, 4);
+sum(-1, -3);
+sum(1, 0);
+sum(1, 1.5);
+sum(2.5, 1.5);
